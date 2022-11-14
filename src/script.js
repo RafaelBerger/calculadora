8c06836f05a4a654e2c851ee9b8bdcee39f4f6ec
Rafael <rafaelberger.dev@gmailcom>
2022-11-13 23:39:50 -0300

    Calculadora aprimorada

---
+++ src/script.js
@@ -0,0 +1,84 @@
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const screen = document.querySelector(".screen_content");
+
+function clearScreen() {
+  screen.innerText = "";
+}
+
+const clearButton = document
+  .querySelector(".clear")
+  .addEventListener("click", () => {
+    clearScreen();
+  });
+
+const del = document.querySelector(".delete").addEventListener("click", () => {
+  const newContent = screen.innerText.slice(0, screen.innerText.length - 1);
+  screen.innerText = newContent;
+});
+
+numbers.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    screen.innerText += e.target.innerText;
+  });
+});
+
+let numbersToBeCalculated = [];
+let operationToBeUsed = undefined;
+let result = undefined;
+
+function clearScreenVariables() {
+  numbersToBeCalculated = [];
+  operationToBeUsed = undefined;
+  result = undefined;
+}
+
+function calculate(operation) {
+  switch (operation) {
+    case "+":
+      result = numbersToBeCalculated.reduce((a, b) => a + b);
+      break;
+    case "-":
+      result = numbersToBeCalculated.reduce((a, b) => a - b);
+      break;
+    case "x":
+      result = numbersToBeCalculated.reduce((a, b) => a * b);
+      break;
+    case "÷":
+      result = numbersToBeCalculated.reduce((a, b) => a / b);
+      break;
+  }
+}
+
+operators.forEach((elements) => {
+  elements.addEventListener("click", (e) => {
+    if (screen.innerText != "") {
+      numbersToBeCalculated.push(parseFloat(screen.innerText));
+    }
+    switch (e.target.innerText) {
+      case "+":
+        operationToBeUsed = "+";
+        clearScreen();
+        break;
+      case "-":
+        operationToBeUsed = "-";
+        clearScreen();
+        break;
+      case "x":
+        operationToBeUsed = "x";
+        clearScreen();
+        break;
+      case "÷":
+        operationToBeUsed = "÷";
+        clearScreen();
+        break;
+      case "=":
+        calculate(operationToBeUsed);
+        if (result != undefined) {
+          screen.innerText = result;
+        }
+        clearScreenVariables();
+        break;
+    }
+  });
+});
